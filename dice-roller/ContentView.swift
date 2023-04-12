2b1fd5ecaad9499814efd2d3bc62b6556f53cd48
Emil <79602784+svidt@users.noreply.github.com>
2023-04-12 21:31:01 +0200

    Update ContentView.swift

--- dice-roller/ContentView.swift
+++ dice-roller/ContentView.swift
@@ -10,9 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "scribble")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+                .padding(20)
             Text("Hello, GitHub!")
         }
         .padding()
