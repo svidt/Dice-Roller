fba50e434587c95dcd2c4251db4706fac4c9f8f2
Emil <79602784+svidt@users.noreply.github.com>
2023-04-12 21:46:27 +0200

    add game code

--- dice-roller/ContentView.swift
+++ dice-roller/ContentView.swift
@@ -8,17 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var dice_image: String = "dice-general"
+    @State var dice_result: String = "Waiting..."
+    
     var body: some View {
-        VStack {
-            Image(systemName: "scribble")
-                .foregroundColor(.yellow)
-                .imageScale(.large)
-                .padding(20)
-            Text("Hello, GitHub!")
+        
+        Form {
+            
+            VStack {
+                
+                Text("Simple Die Roller App")
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                    .padding(.bottom, 25)
+                
+                Image(self.dice_image)
+                    .resizable().frame(width: 300, height: 300)
+                
+                Text(self.dice_result)
+                    .font(.title)
+                    .padding(.bottom, 25)
+                 
+                Button(action: {
+                    
+                    let randomNumber = arc4random_uniform(6) + 1
+                    self.dice_result = String(randomNumber)
+                    
+                    switch randomNumber {
+                    case 1:
+                        self.dice_image = "dice-1"
+                    case 2:
+                        self.dice_image = "dice-2"
+                    case 3:
+                        self.dice_image = "dice-3"
+                    case 4:
+                        self.dice_image = "dice-4"
+                    case 5:
+                        self.dice_image = "dice-5"
+                    case 6:
+                        self.dice_image = "dice-6"
+                    default:
+                        return
+                    }
+                    
+                }) {
+                    Text("Roll")
+                        .font(.title)
+                        .bold()
+                        .padding(25)
+                }
+            }
         }
-        .padding()
     }
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
